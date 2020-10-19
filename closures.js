fc011a8c05326a90185669c3c768e790db14ac96
cdavis85 <christydavis1123@gmail.com>
2020-10-19 16:43:37 -0500

    "commit for progress"

--- closures.js
+++ closures.js
@@ -22,13 +22,13 @@ function outer() {
   Invoke outer saving the return value into another variable called 'inner'.
 */
   
-// Code Here
+let inner = outer();
 
 
 
 //Once you do that, invoke inner.
 
-//Code Here
+inner();
 
 
 
@@ -51,7 +51,10 @@ function callFriend(name) {
   (HINT: You will need to pass in arguments to both function invocations)
 */
 
-//Code Here
+let callJake = callFriend('Jake');
+callJake('Jake');
+
+dial(435-555-9248);
 
 
 
@@ -61,16 +64,22 @@ function callFriend(name) {
   Write a function called makeCounter that makes the following code work properly.
 */
 
-//Code Here
+function makeCounter(){
+let num = 0;
+function add(){
+  return ++num;
+}
+return add;
+}
 
 
 
 //Uncomment this once you make your function
-//   var count = makeCounter();
-//   count(); // 1
-//   count(); // 2
-//   count(); // 3
-//   count(); // 4
+  var count = makeCounter();
+   count(); // 1
+   count(); // 2
+   count(); // 3
+   count(); // 4
 
 
 
@@ -86,7 +95,7 @@ function callFriend(name) {
 */
 
 function counterFactory(value) {
-  // Code here.
+function inc()
 
   return {
 
